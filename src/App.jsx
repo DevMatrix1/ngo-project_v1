34c66dc94fece320558964a8136daa624165a544
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2023-02-17 13:02:58 +0530

    Done with Scaffolding of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import React from "react";
+import Home from "./pages/Home/Home";
+import Maintenance from "./pages/Maintenance/Maintenance";
+
+const App = () => {
+  return (
+    <Home />
+    // <Maintenance/>
+  );
+};
+
+export default App;
