ca6fabdc169e6309c291bfe90d9cf2102a14f88e
Ankit Kumar <ak9664773@gmail.com>
2023-02-21 12:37:51 +0530

    Added Maintenance Page & did little-bit change in Navbar height

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 
 import React from "react";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
+import { Navbar } from "./components/Navbar/Navbar";
+import Maintenance from "./pages/Maintenance/Maintenance";
 
 const App = () => {
   return (
-    <Home />
-    // <Maintenance/>
+    <>
+      {/* <Home /> */}
+      <Navbar />
+      <Maintenance/>
+    </>
   );
 };
 
